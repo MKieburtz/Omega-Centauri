e3fde05c40220af0e17f6aa54773a017a59d8231
codeNinja77 <hightechninja7@gmail.com>
2014-03-10 13:24:19 -0700

    Transparent launcher background (really)

--- OmegaCentauri/src/MainPackage/Launcher.java
+++ OmegaCentauri/src/MainPackage/Launcher.java
@@ -25,7 +25,6 @@ public class Launcher extends JFrame implements MouseListener {
     private java.util.Timer refreshTimer = new java.util.Timer();
     private boolean fullScreen = false;
     private Settings settings;
-    private DrawingPanel panel = new DrawingPanel();
     private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
     GraphicsDevice graphicsDevice = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
@@ -43,37 +42,47 @@ public class Launcher extends JFrame implements MouseListener {
 
         settings = new Settings(screenSize);
 
-        setUpWindow(width, height);
-    }
-
-    private void setUpWindow(int width, int height) {
-
-        setIconImage(images.get(1));
-        setSize(width, height);
-        setUndecorated(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        setSize(screenSize);
+        //setLocationRelativeTo(null);
+        setUndecorated(true);
         setBackground(new Color(0,255,0,0));
-        setLayout(new BorderLayout());
-
-        addMouseListener(this);
-        setTitle("Omega Centauri Launcher");
-        setResizable(false);
-        setLocationRelativeTo(null);
         
-        addComponents();
-        requestFocus();
-        setVisible(true);
-
-    }
-
-    private void addComponents() {
         setContentPane(new BackPanel());
         getContentPane().setBackground(Color.BLACK);
         setLayout(new BorderLayout());
         
-        panel.setSize(screenSize);
+        DrawingPanel panel = new DrawingPanel();
         add(panel);
+        
+        setVisible(true);
+        
+        //setUpWindow(width, height);
+    }
+
+    private void setUpWindow(int width, int height) {
+
+        //setIconImage(images.get(1));
+        //setSize(width, height);
+        //setUndecorated(true);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //setBackground(new Color(0,255,0,0));
+        //setLayout(new BorderLayout());
+
+        //addMouseListener(this);
+        //setTitle("Omega Centauri Launcher");
+        //setResizable(false);
+        //setLocationRelativeTo(null);
+        
+        //addComponents();
+        //requestFocus();
+        //setVisible(true);
+
+    }
+
+    private void addComponents() {
+        
     }
 
     private void setWindowFullScreen() {
@@ -85,34 +94,33 @@ public class Launcher extends JFrame implements MouseListener {
 
         public BackPanel() {
             setOpaque(false);
-            setVisible(true);
         }
 
         @Override
         protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            
-        Graphics2D overGraphics2D = (Graphics2D)g.create();
-        
-        overGraphics2D.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.0f));
-        overGraphics2D.setColor(Color.BLACK);
-        overGraphics2D.fill(getBounds());
-        overGraphics2D.dispose();
+        // Allow super to paint
+        super.paintComponent(g);
+        System.out.println("backPanel drew");
+        // Apply our own painting effect
+        Graphics2D g2d = (Graphics2D) g.create();
+        // 50% transparent Alpha
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.0f));
+
+        g2d.setColor(getBackground());
+        g2d.fill(getBounds());
+
+        g2d.dispose();
         }
     }
     
     public class DrawingPanel extends JPanel
     {
-        public DrawingPanel()
-        {
-            setVisible(true);
-        }
-        
         @Override
         protected void paintComponent(Graphics g)
         {
-            super.paintComponent(g);
-            refreshTimer.schedule(new refreshTimer(), 1);
+            //super.paintComponent(g);
+            //refreshTimer.schedule(new refreshTimer(), 1);
+            renderer.drawLauncher(g.create(), images.get(0), images.get(2), images.get(images.size() - 2), images.get(4)); // use active rendering            
         }
     }
 
@@ -174,10 +182,10 @@ public class Launcher extends JFrame implements MouseListener {
 
         @Override
         public void run() {
-            if (panel.getGraphics() != null)
-                renderer.drawLauncher(panel.getGraphics(), images.get(0), images.get(2), images.get(images.size() - 2), images.get(4)); // use active rendering
-            
-            refreshTimer.schedule(new refreshTimer(), 100);
+//            if (panel.getGraphics() != null)
+//                renderer.drawLauncher(panel.getGraphics(), images.get(0), images.get(2), images.get(images.size() - 2), images.get(4)); // use active rendering
+//            
+//            refreshTimer.schedule(new refreshTimer(), 100);
         }
     }
 }
--- OmegaCentauri/src/OmegaCentauri.java
+++ OmegaCentauri/src/OmegaCentauri.java
@@ -1,20 +1,27 @@
 
 import MainPackage.EDTExceptionHandler;
 import MainPackage.Launcher;
+import javax.swing.SwingUtilities;
 
 /**
-* @author Michael Kieburtz
-* @version Dev: 1.0.2
-*/
+ * @author Michael Kieburtz
+ * @version Dev: 1.0.2
+ */
 public class OmegaCentauri {
 
     static Launcher launcher;
 
     public static void main(String args[]) {
-        
-        Thread.setDefaultUncaughtExceptionHandler(new EDTExceptionHandler());
-        System.setProperty("sun.awt.exception.handler", EDTExceptionHandler.class.getName());
-        
-        launcher = new Launcher();
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                Thread.setDefaultUncaughtExceptionHandler(new EDTExceptionHandler());
+                System.setProperty("sun.awt.exception.handler", EDTExceptionHandler.class.getName());
+
+                new Launcher();
+            }
+        });
+
     }
 }
