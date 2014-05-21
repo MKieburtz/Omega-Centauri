450cdc4df147d8cc47a1c0f07a785b6130015333
codeNinja77 <hightechninja7@gmail.com>
2014-05-21 13:15:40 -0700

    Fixed up some hovering stuff

--- OmegaCentauri/src/MainPackage/MainMenu.java
+++ OmegaCentauri/src/MainPackage/MainMenu.java
@@ -22,6 +22,7 @@ public class MainMenu implements MouseListener, MouseMotionListener {
     private ArrayList<String> soundPaths = new ArrayList<String>();
     
     private ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+    private ArrayList<BufferedImage> subimages = new ArrayList<BufferedImage>();
     private ArrayList<Clip> sounds = new ArrayList<Clip>();
     
     private ArrayList<TwinklingStarChunk> stars = new ArrayList<TwinklingStarChunk>();
@@ -30,7 +31,7 @@ public class MainMenu implements MouseListener, MouseMotionListener {
             
     private int START = 0;
     private final int STARTHOVER = 1; //filler so hover doesn't turn into the close button :3
-    private final int FULLSTART = 2;
+    private final int HOVERTILES = 2;
     private final int CLOSE = 3;
     
     private int hovered = 0;
@@ -45,6 +46,8 @@ public class MainMenu implements MouseListener, MouseMotionListener {
     private Point size;
     private Dimension screenSize;
     
+    private Rectangle screenRect;
+    
     public MainMenu(OmegaCentauri game)
     {
         active = true;
@@ -56,12 +59,14 @@ public class MainMenu implements MouseListener, MouseMotionListener {
         imagePaths.add("src/resources/StartButtonTileframe.png");
         imagePaths.add("src/resources/CloseButton.png");
         images = loader.loadImages(imagePaths);
-        
+        loadSubimages();
         soundPaths.add("src/resources/mouseClick.wav");
         sounds = loader.loadSounds(soundPaths);
         
         screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         size = new Point(game.getWidth(), game.getHeight());
+        
+        screenRect = new Rectangle(0, 0, size.x, size.y);
         
         // load enough stars for the entire screen;
         for (int x = 0; x < screenSize.width; x += 100)
@@ -77,20 +82,27 @@ public class MainMenu implements MouseListener, MouseMotionListener {
                 size.y - 13 - images.get(CLOSE).getHeight() * 2, images.get(CLOSE).getWidth(), images.get(CLOSE).getHeight());
     }
     
+    private void loadSubimages()
+    {
+        for (int i = 0; i < 3600; i+=200)
+        {
+            subimages.add(images.get(HOVERTILES).getSubimage(i, 0, 200, 100));
+        }
+    }
+    
     public void draw(Graphics g)
     {
         BufferedImage drawingImage = new BufferedImage(screenSize.width, screenSize.height, BufferedImage.TYPE_INT_ARGB);
         
         Graphics2D g2d = drawingImage.createGraphics();
         
-        
-        
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, size.x, size.y);
         
         for (TwinklingStarChunk s : stars)
         {
-            s.draw(g2d);
+            if (s.getBoundingRect().intersects(screenRect))
+                s.draw(g2d);
         }
         g2d.setColor(Color.CYAN);
         g2d.drawLine(0, size.y - 140, size.x, size.y - 140);
@@ -98,16 +110,17 @@ public class MainMenu implements MouseListener, MouseMotionListener {
         g2d.fillRect(0, size.y - 139, size.x, size.y - 139);
        
         g2d.setColor(Color.RED);
+                
+        g2d.drawImage(hovering ? hovered == subimages.size() - 1 ? images.get(STARTHOVER) : subimages.get(hovered): images.get(START), 0, size.y - 36 - images.get(START).getHeight() - 3, null);
         
+        if (hovered < subimages.size() - 1 && hovering)
+            hovered += 1;
         
-        g2d.drawImage(hovering ? images.get(FULLSTART).getSubimage(hovered, 0, 200, 100): images.get(START), 0, size.y - 36 - images.get(START).getHeight() - 3, null);
-        if (hovered < 3600)
-            MainMenu.this.hovered += 200;
         g2d.drawImage(images.get(CLOSE), size.x - 30 - images.get(CLOSE).getWidth(), 
                 size.y - 13 - images.get(CLOSE).getHeight() * 2, null);
                         
-        g2d.draw(startRectangle);
-        g2d.draw(closeRectangle);
+//        g2d.draw(startRectangle);
+//        g2d.draw(closeRectangle);
         
         g.drawImage(drawingImage, 0, 0, null);
     }
@@ -145,6 +158,7 @@ public class MainMenu implements MouseListener, MouseMotionListener {
     @Override
     public void mouseExited(MouseEvent e) {
         hovering = false;
+        hovered = 0;
     }
 
         @Override
@@ -156,10 +170,10 @@ public class MainMenu implements MouseListener, MouseMotionListener {
         if (startRectangle.contains(me.getPoint()))
         {
             hovering = true;
-        } else if (!startRectangle.contains(me.getPoint()))
+        } else
         {
             hovering = false;
-            MainMenu.this.hovered = 0;
+            hovered = 0;
         }
     }
     
@@ -190,5 +204,6 @@ public class MainMenu implements MouseListener, MouseMotionListener {
         startRectangle.setBounds(0, size.y - 30 - images.get(START).getHeight() - 3, images.get(START).getWidth(), images.get(START).getHeight());
         closeRectangle.setBounds(size.x - 30 - images.get(CLOSE).getWidth(),
                 size.y - 13 - images.get(CLOSE).getHeight() * 2, images.get(CLOSE).getWidth(), images.get(CLOSE).getHeight());
+        screenRect.setBounds(0, 0, size.x, size.y);
     }
 }
