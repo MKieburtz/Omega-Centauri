86f48d4677464922ccf55547630b5b2d136876b5
codeNinja77 <hightechninja7@gmail.com>
2014-05-28 13:02:02 -0700

    Revert "Performance update"
    This reverts commit fede9640323bfef0ed3e02178c0ba38816fe2afd.

--- OmegaCentauri/src/MainPackage/Calculator.java
+++ OmegaCentauri/src/MainPackage/Calculator.java
@@ -2,7 +2,6 @@ package MainPackage;
 
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
 
 /**
  * @author Michael Kieburtz
@@ -14,6 +13,7 @@ public class Calculator {
      * calculate stuff
      */
 
+
     public static double CalcAngleMoveX(double angle) {
         return Math.cos(Math.toRadians(angle));
     }
@@ -21,7 +21,6 @@ public class Calculator {
     public static double CalcAngleMoveY(double angle) {
         return Math.sin(Math.toRadians(angle));
     }
-
     /**
      * @returns distance between two points
      */
@@ -31,9 +30,8 @@ public class Calculator {
 
     /**
      * @returns angle between two points IN DEGREES
-     */
+     */ 
     static double angle;
-
     public static double getAngleBetweenTwoPoints(Point2D.Double pt1, Point2D.Double pt2) {
         angle = (double) Math.toDegrees(Math.atan2(pt2.x - pt1.x, pt2.y - pt1.y));
 
@@ -43,70 +41,55 @@ public class Calculator {
 
         return angle;
     }
-
-    public static boolean collisionCheck(Rectangle2D.Double hitbox1, Rectangle2D.Double hitbox2) {
-        return hitbox1.intersects(hitbox2);
+    
+    public static boolean collisionCheck(Rectangle2D.Double hitbox1,Rectangle2D.Double hitbox2)
+    {
+            return hitbox1.intersects(hitbox2);
     }
-
-    public static double[] getDistancesBetweenAngles(double angle1, double angle2) {
+    
+    public static double[] getDistancesBetweenAngles(double angle1, double angle2)
+    {
         if (angle1 > angle2) {
             if (angle1 + 180 >= 360) // CASE 1
             {
-                return new double[]{Math.abs((360 - angle1) + angle2), Math.abs(angle1 - angle2)};
+                return new double[] { Math.abs((360 - angle1) + angle2), Math.abs(angle1 - angle2) };
             } else if (angle1 + 180 < 360) // CASE 2
             {
-                return new double[]{Math.abs((360 - angle2) + angle1), Math.abs(angle2 - angle1)};
+                return new double[] { Math.abs((360 - angle2) + angle1), Math.abs(angle2 - angle1) };
             }
         } else if (angle2 > angle1) {
             if (angle2 + 180 >= 360) // CASE 3
             {
-                return new double[]{Math.abs(angle2 - angle1), Math.abs(360 - (angle2 - angle1))};
+                return new double[] { Math.abs(angle2 - angle1), Math.abs(360 - (angle2 - angle1)) };
             } else if (angle2 + 180 < 360) // CASE 4
             {
-                return new double[]{Math.abs(angle2 - angle1), Math.abs(360 - (angle2 - angle1))};
+                return new double[] { Math.abs(angle2 - angle1), Math.abs(360 - (angle2 - angle1)) };
             }
         }
-
-        return new double[]{0.0, 360};
+        
+        return new double[] {0.0, 360};
     }
-
+    
     public static Point2D.Double getScreenLocation(Point2D.Double cameraLocation, Point2D.Double location) {
         double x = location.x - cameraLocation.x;
         double y = location.y - cameraLocation.y;
 
         return new Point2D.Double(x, y);
     }
-
+    
     public static Point2D.Double getScreenLocationMiddle(Point2D.Double cameraLocation, Point2D.Double location,
             double imageWidth, double imageHeight) {
         double x = location.x - cameraLocation.x + (imageWidth / 2);
         double y = location.y - cameraLocation.y + (imageHeight / 2);
-
+        
         return new Point2D.Double(x, y);
     }
-
+    
     public static Point2D.Double getScreenLocationMiddleForPlayer(Point2D.Double cameraLocation, Point2D.Double location,
             double imageWidth, double imageHeight) {
         double x = getScreenLocation(cameraLocation, location).x + cameraLocation.x + imageWidth / 2;
         double y = getScreenLocation(cameraLocation, location).y + cameraLocation.y + imageHeight / 2;
 
         return new Point2D.Double(x, y);
-    }
-
-    public static int getAverage(ArrayList<Integer> values) {
-        if (!values.isEmpty()) {
-            int total = 0;
-            for (Integer i : values) {
-                total += i;
-            }
-
-            return total / values.size();
-        }
-        return 0;
-    }
-    
-    public static boolean closeToNumber(int targetNumber, int number, int buffer)
-    {
-        return Math.abs(number - targetNumber) <= buffer;
     }
 }
--- OmegaCentauri/src/MainPackage/OmegaCentauri.java
+++ OmegaCentauri/src/MainPackage/OmegaCentauri.java
@@ -28,10 +28,8 @@ public class OmegaCentauri extends Game implements GameStartListener {
     private int FPS = 0;
     private int UPS = 0;
     private int updates = 0;
-    private long loopTimeUPS = (long) ((1000.0 / 77) * 1000000); // about 13. Change 77 for the target UPS
+    private final long loopTimeUPS = (long) ((1000.0 / 77) * 1000000); // about 13. Change 75 for the target UPS
     private int framesDrawn = 0;
-    private ArrayList<Integer> UpdateList = new ArrayList<Integer>();
-    private int averageUPS = -1;
     /*
      * OBJECTS:
      */
@@ -78,18 +76,20 @@ public class OmegaCentauri extends Game implements GameStartListener {
         setTitle("Omega Centauri");
         setMinimumSize(new Dimension(600, 600));
 
+        
+
         setBackground(Color.BLACK);
 
         setSize(1000, 600);
-
+        
         mainMenu = new MainMenu(this);
 
         panel = new Panel(1000, 600);
-
+        
         setInputMaps();
 
         getContentPane().add(panel);
-
+        
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent we) {
@@ -99,15 +99,6 @@ public class OmegaCentauri extends Game implements GameStartListener {
                 } catch (java.awt.IllegalComponentStateException ex) {
                     System.err.println("ERROR!");
                 }
-            }
-        });
-
-        addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent event)
-            {
-                averageUPS = 0;
-                UpdateList.clear();
             }
         });
 
@@ -270,8 +261,7 @@ public class OmegaCentauri extends Game implements GameStartListener {
     }
 
     private void gameUpdate() {
-        //long start = System.currentTimeMillis();
-
+        
         if (!paused) {
 
             if (camera.getSize().x != getWidth() || camera.getSize().y != getHeight()) {
@@ -359,8 +349,6 @@ public class OmegaCentauri extends Game implements GameStartListener {
 
             syncGameStateVaribles();
         }
-//        long end = System.currentTimeMillis();
-//        System.out.println(end - start);
     }
 
     /*
@@ -517,26 +505,12 @@ public class OmegaCentauri extends Game implements GameStartListener {
 
         @Override
         public void run() {
-            FPS = framesDrawn * 4;
-            UPS = updates * 4;
+            FPS = framesDrawn;
+            UPS = updates;
             framesDrawn = 0;
             updates = 0;
 
-            UpdateList.add(UPS < 30 ? UPS * 2 : UPS);
-            if (UpdateList.size() > 30) {
-                UpdateList.remove(0);
-            }
-            
-            averageUPS = Calculator.getAverage(UpdateList);
-            if (averageUPS < 70 && averageUPS != -1) {
-                if (!Calculator.closeToNumber(70, averageUPS, 2))
-                    loopTimeUPS -= 20000;
-            } else if (averageUPS > 70 && averageUPS != -1) {
-                if (!Calculator.closeToNumber(70, averageUPS, 2))
-                    loopTimeUPS += 20000;
-            }
-            System.out.println(loopTimeUPS + " " + averageUPS);
-            recordingEx.schedule(new RecordingService(), 250, TimeUnit.MILLISECONDS);
+            recordingEx.schedule(new RecordingService(), 1, TimeUnit.SECONDS);
         }
     }
 
@@ -560,7 +534,7 @@ public class OmegaCentauri extends Game implements GameStartListener {
                             renderer.drawScreen(panel.getGraphics(), shipsToDraw, middleOfPlayer.x, middleOfPlayer.y,
                                     FPS, stars, camera, Version, UPS, paused);
                             framesDrawn++;
-
+                            
                             if (!OmegaCentauri.this.hasFocus()) {
                                 paused = true;
                             }
@@ -669,7 +643,7 @@ public class OmegaCentauri extends Game implements GameStartListener {
                             loading = false;
 
                         }
-
+                        
                         if (loading) {
                             timingEx.schedule(new LoadingService(), 3, TimeUnit.MILLISECONDS);
                         } else {
@@ -677,7 +651,7 @@ public class OmegaCentauri extends Game implements GameStartListener {
                             shipsToDraw.addAll(enemyShips);
                             shipsToDraw.addAll(allyShips);
 
-                            recordingEx.schedule(new RecordingService(), 250, TimeUnit.MILLISECONDS);
+                            recordingEx.schedule(new RecordingService(), 1, TimeUnit.SECONDS);
                             timingEx.schedule(new UpdatingService(), 1, TimeUnit.NANOSECONDS);
                         }
 
@@ -699,12 +673,13 @@ public class OmegaCentauri extends Game implements GameStartListener {
                 public void run() {
                     try {
                         if (mainMenu.isActive()) {
-
-                            if (mainMenu.getSize().x != OmegaCentauri.this.getWidth()
-                                    || mainMenu.getSize().y != OmegaCentauri.this.getHeight()) {
+                            
+                            if (mainMenu.getSize().x != OmegaCentauri.this.getWidth() || 
+                                    mainMenu.getSize().y != OmegaCentauri.this.getHeight())
+                            {
                                 mainMenu.setSize(OmegaCentauri.this.getWidth(), OmegaCentauri.this.getHeight());
                             }
-
+                            
                             mainMenu.draw(panel.getGraphics());
                             timingEx.schedule(new MainMenuService(), 15, TimeUnit.MILLISECONDS);
                         }
@@ -742,6 +717,7 @@ public class OmegaCentauri extends Game implements GameStartListener {
 //        System.setProperty("sun.java2d.opengl", "true");
 //        System.setProperty("sun.java2d.ddscale", "true");
 //        System.setProperty("sun.java2d.translaccel", "true");
+        
         
         new OmegaCentauri();
     }
