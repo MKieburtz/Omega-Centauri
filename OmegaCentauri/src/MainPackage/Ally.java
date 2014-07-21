e0f27423d11c42b9faa124b9bc9cfee7fcd8fa7a
codeNinja77 <hightechninja7@gmail.com>
2014-07-20 17:02:53 -0700

    Added turret class

--- OmegaCentauri/src/MainPackage/Ally.java
+++ OmegaCentauri/src/MainPackage/Ally.java
@@ -1,5 +1,7 @@
 package MainPackage;
 
+import java.awt.geom.Point2D;
+
 /**
  * @author Michael Kieburtz
  * @author Davis Freeman
@@ -10,5 +12,9 @@ public class Ally extends Ship {
             double angleIncrement, double acceleration, int shootingDelay, int health) {
         super(x, y, shipType, baseMaxVel, maxVel, angleIncrement, acceleration, shootingDelay, 
              health);
+    }
+
+    @Override
+    public void shoot(Point2D.Double cameraLocation) {
     }
 }
---
+++ OmegaCentauri/src/MainPackage/Turret.java
@@ -0,0 +1,45 @@
+package MainPackage;
+
+import java.awt.Graphics2D;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.util.*;
+
+/**
+ * @author Kieburtz
+ */
+public class Turret {
+
+    private ArrayList<String> imagePaths = new ArrayList<String>();
+    
+    private ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+    
+    private MediaLoader loader = new MediaLoader();
+    
+    private int maxDurability;
+    private int durability;
+    
+    private int maxRotation;
+    private int minRotation;
+    
+    private int angle = 0;
+    
+    public Turret(int maxDurability, int maxRotation, int minRotation)
+    {
+        this.maxDurability = maxDurability;
+        this.durability = maxDurability;
+        
+        this.maxRotation = maxRotation;
+        this.minRotation = minRotation;
+    }
+    
+    public void draw(Graphics2D g2d, Point2D.Double cameraLocation)
+    {
+        
+    }
+    
+    public void update(int angleToRotate)
+    {
+        
+    }
+}
