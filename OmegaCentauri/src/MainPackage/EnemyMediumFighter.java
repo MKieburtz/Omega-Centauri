5adaaefced7e67e1e56586b244ecc815deba2ec3
codeNinja77 <hightechninja7@gmail.com>
2014-07-31 16:36:19 -0700

    Working on turrets

--- OmegaCentauri/src/MainPackage/EnemyMediumFighter.java
+++ OmegaCentauri/src/MainPackage/EnemyMediumFighter.java
@@ -1,5 +1,6 @@
 package MainPackage;
 
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.Point2D;
@@ -9,62 +10,68 @@ import java.util.ArrayList;
 /**
  * @author Michael Kieburtz
  */
-public class EnemyMediumFighter  extends EnemyShip {
-    
+public class EnemyMediumFighter extends EnemyShip {
+
     private int id;
-    
-    
+
+    private Turret[] turrets = new Turret[2];
+
     public EnemyMediumFighter(int x, int y, Type shipType, double baseMaxVel, double maxVel, double maxAngleVelocity,
             double angleIncrement, double acceleration, Point2D.Double cameraLocation,
-            int shootingDelay, int health, int id)
-    {
+            int shootingDelay, int health, int id) {
         super(x, y, shipType, baseMaxVel, maxVel, maxAngleVelocity, angleIncrement, acceleration, shootingDelay, health);
-        
+
         this.id = id;
-        
+
         imagePaths.add("resources/MediumEnemyFighter.png");
-        
+
         images = Calculator.toCompatibleImages(mediaLoader.loadImages(imagePaths));
         activeImage = images.get(0);
-        
+
         shield = new Shield(faceAngle, location, new Point2D.Double(0, 0), true, new Point(activeImage.getWidth(), activeImage.getHeight()), 15, 50);
 
         setUpHitbox(cameraLocation);
-        
+
+        turrets[0] = new Turret(25, 135, 45, new Point2D.Double(-100, 0), new Dimension(activeImage.getWidth(), activeImage.getHeight()));
+        turrets[1] = new Turret(25, 135, 45, new Point2D.Double(100, 0), new Dimension(activeImage.getWidth(), activeImage.getHeight()));
     }
 
     @Override
     public void shoot(Point2D.Double cameraLocation) {
     }
-   
-    
+
     @Override
-    public void draw(Graphics2D g2d, Camera camera)
-    {
+    public void draw(Graphics2D g2d, Camera camera) {
         super.draw(g2d, camera);
+
+        for (Turret t : turrets) {
+            t.draw(g2d, camera.getLocation(), location);
+        }
     }
 
     @Override
     public void update(Player player, Point2D.Double cameraLocation, ArrayList<EnemyShip> otherShips) {
-        
+
         double distance = Calculator.getDistance(location, player.getLocation());
-        
+
         double angleToPlayer = Calculator.getAngleBetweenTwoPoints(location, player.getLocation());
-        
-        rotateToAngle(angleToPlayer);
-        
-        move(ShipState.Thrusting);
+
+        //rotateToAngle(angleToPlayer);
+        for (Turret t : turrets) {
+            t.update(player.getLocation(), location, faceAngle);
+        }
+
+        //move(ShipState.Thrusting);
     }
 
     @Override
     public int getID() {
         return id;
     }
-    
+
     @Override
-    public Point2D.Double getLocation()
-    {
+    public Point2D.Double getLocation() {
         return location;
     }
-    
+
 }
--- OmegaCentauri/src/MainPackage/Turret.java
+++ OmegaCentauri/src/MainPackage/Turret.java
@@ -1,6 +1,9 @@
 package MainPackage;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.geom.*;
 import java.awt.image.BufferedImage;
 import java.util.*;
@@ -12,6 +15,7 @@ public class Turret {
 
     private ArrayList<String> imagePaths = new ArrayList<String>();
     
+    private BufferedImage activeImage;
     private ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
     
     private MediaLoader loader = new MediaLoader();
@@ -22,9 +26,16 @@ public class Turret {
     private int maxRotation;
     private int minRotation;
     
-    private int angle = 0;
+    private double angle;
     
-    public Turret(int maxDurability, int maxRotation, int minRotation)
+    private final Point2D.Double distanceFromCenter;
+    private Dimension imageDimensions;
+    
+    private Point2D.Double location = new Point2D.Double();
+    
+    private final int TURRETIMAGE = 0;
+    
+    public Turret(int maxDurability, int maxRotation, int minRotation, Point2D.Double distanceFromCenter, Dimension imageDimensions)
     {
         this.maxDurability = maxDurability;
         this.durability = maxDurability;
@@ -32,20 +43,54 @@ public class Turret {
         this.maxRotation = maxRotation;
         this.minRotation = minRotation;
         
+        angle = (maxRotation + minRotation) / 2; // average
+        
+        this.distanceFromCenter = distanceFromCenter;
+        this.imageDimensions = imageDimensions;
+        
         imagePaths.add("resources/Turret.png");
         
         images = loader.loadImages(imagePaths);
         
         images = Calculator.toCompatibleImages(images);
+        
+        activeImage = images.get(TURRETIMAGE);
     }
     
-    public void draw(Graphics2D g2d, Point2D.Double cameraLocation)
+    public void draw(Graphics2D g2d, Point2D.Double cameraLocation, Point2D.Double entityLocation)
     {
+        Point2D.Double middle = Calculator.getScreenLocationMiddle(cameraLocation, location, activeImage.getWidth(), activeImage.getHeight());
+        AffineTransform transform = g2d.getTransform();
+//        
+//        transform.translate(Calculator.getScreenLocation(cameraLocation, location).x, Calculator.getScreenLocation(cameraLocation, location).y);
         
+        //transform.rotate(Math.toRadians(360 - angle), middle.x, middle.y);
+        
+        g2d.drawImage(images.get(TURRETIMAGE), transform, null);
+        
+        g2d.setColor(Color.RED);
+        
+        g2d.drawRect((int)Calculator.getScreenLocation(cameraLocation, location).x, (int)Calculator.getScreenLocation(cameraLocation, location).y, 10, 10);
+       
+        g2d.drawRect((int)middle.x, (int)middle.y, 20, 20);
     }
     
-    public void update(int angleToRotate)
+    public void update(Point2D.Double playerLocation, Point2D.Double shipLocation, double shipAngle)
     {
+        Point2D.Double middle = new Point2D.Double(shipLocation.x + activeImage.getWidth() / 2, shipLocation.y + activeImage.getHeight() / 2);
         
+        double targetAngle = Math.abs(Calculator.getAngleBetweenTwoPoints(shipLocation, playerLocation) - shipAngle) % 360;
+        
+        if (targetAngle > minRotation && targetAngle < maxRotation)
+        {
+            angle = targetAngle;
+        }
+        
+        
+        
+        location.x = middle.x + distanceFromCenter.x;
+        location.y = middle.y + distanceFromCenter.y;
+        
+        System.out.println(middle + " " + distanceFromCenter + " " + location);
     }
 }
