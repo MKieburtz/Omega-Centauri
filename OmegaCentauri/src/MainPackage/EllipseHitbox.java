24dd9fe74a600a1335cc5ff08d41151b8e13dbb1
codeNinja77 <hightechninja7@gmail.com>
2014-10-30 17:08:03 -0700

    Finished the new collision checking code

--- OmegaCentauri/src/MainPackage/EllipseHitbox.java
+++ OmegaCentauri/src/MainPackage/EllipseHitbox.java
@@ -10,56 +10,50 @@ import java.awt.geom.Point2D;
  * @author Michael Kieburtz
  */
 public class EllipseHitbox {
-    
+
     private Point2D.Double centerPoint = new Point2D.Double();
     // this is the longer "radius" length
     private double semiMajorAxisLength;
     // this is the shorter "radius" length
     private double semiMinorAxisLength;
-    
+
     private double horizontalRadiusLength;
     private double verticalRadiusLength;
-    
+
     private double angle = 0;
-    
+
     private boolean circle;
-    
-    public EllipseHitbox(double horizontalLength, double verticalLength)
-    {
+
+    public EllipseHitbox(double horizontalLength, double verticalLength) {
         this.horizontalRadiusLength = horizontalLength;
         this.verticalRadiusLength = verticalLength;
-        
+
         this.semiMajorAxisLength = horizontalLength >= verticalLength ? horizontalLength / 2 : verticalLength / 2;
         this.semiMinorAxisLength = semiMajorAxisLength == verticalLength ? horizontalLength / 2 : verticalLength / 2;
-        
+
         circle = semiMajorAxisLength == semiMinorAxisLength;
     }
-    
-    public void rotateToAngle(double angle)
-    {
+
+    public void rotateToAngle(double angle) {
         this.angle = angle;
         this.angle = Calculator.confineAngleToRange(this.angle);
     }
-    
-    public void rotateRelative(double amount)
-    {
+
+    public void rotateRelative(double amount) {
         angle += amount;
         angle = Calculator.confineAngleToRange(angle);
     }
-    
-    public void moveToLocation(Point2D.Double location)
-    {
+
+    public void moveToLocation(Point2D.Double location) {
         this.centerPoint = location;
     }
-    
-    public void moveRelative(Point2D.Double distance)
-    {
+
+    public void moveRelative(Point2D.Double distance) {
         centerPoint.x += distance.x;
         centerPoint.y += distance.y;
     }
-    
-    public void draw(Graphics2D g2d)
-    {
+
+    public void draw(Graphics2D g2d) {
         Line2D.Double line1 = new Line2D.Double(centerPoint.x - horizontalRadiusLength, centerPoint.y, centerPoint.x, centerPoint.y - verticalRadiusLength);
         Line2D.Double line2 = new Line2D.Double(centerPoint.x, centerPoint.y - verticalRadiusLength, centerPoint.x + horizontalRadiusLength, centerPoint.y);
         Line2D.Double line3 = new Line2D.Double(centerPoint.x + horizontalRadiusLength, centerPoint.y, centerPoint.x, centerPoint.y + verticalRadiusLength);
@@ -69,41 +63,53 @@ public class EllipseHitbox {
         g2d.draw(line3);
         g2d.draw(line4);
     }
-    
-    public boolean collides(RectangularHitbox other)
-    {
+
+    public boolean collides(RectangularHitbox other) {
         Point2D.Double rotatedCenter = Calculator.rotatePointAroundPoint(centerPoint, other.getCenterPoint(), -other.getAngle());
-       
-        return true;
+
+        Point2D.Double closestRectPoint = getClosestPointOnEdgeOfRectangle(other.getTopLeftPoint().x,
+                other.getTopLeftPoint().y, other.getDimensions().width, other.getDimensions().height, rotatedCenter);
+
+        if (!circle) {
+            closestRectPoint = Calculator.rotatePointAroundPoint(closestRectPoint, centerPoint, -angle);
+
+            return pointInsideEllipse(centerPoint, semiMajorAxisLength, semiMinorAxisLength, closestRectPoint);
+        } else {
+            return Calculator.getDistance(closestRectPoint, centerPoint) < semiMajorAxisLength; // semiMajor == semiMinor so we could use either
+        }
     }
-    
-    private Point2D.Double getClosestPointOnEdgeOfRectangle(double rectX, double rectY, double rectwidth, double rectHeight, Point2D.Double location)
-    {
+
+    private Point2D.Double getClosestPointOnEdgeOfRectangle(double rectX, double rectY, double rectwidth, double rectHeight, Point2D.Double location) {
         double right = rectX + rectwidth;
         double bottom = rectY + rectHeight;
-        
-        double x  = Calculator.clamp(location.x, rectX, right);
+
+        double x = Calculator.clamp(location.x, rectX, right);
         double y = Calculator.clamp(location.y, rectY, bottom);
-        
+
         double distanceLeft = Math.abs(x - rectX);
         double distanceRight = Math.abs(x - right);
         double distanceTop = Math.abs(y - rectY);
         double distanceBottom = Math.abs(y - bottom);
-        
+
         double min = Calculator.min(distanceLeft, distanceRight, distanceTop, distanceBottom);
-        
-        if (min == distanceTop) return new Point2D.Double(x, rectY);
-        if (min == distanceBottom) return new Point2D.Double(x, bottom);
-        if (min == distanceLeft) return new Point2D.Double(rectX, y);
-        
+
+        if (min == distanceTop) {
+            return new Point2D.Double(x, rectY);
+        }
+        if (min == distanceBottom) {
+            return new Point2D.Double(x, bottom);
+        }
+        if (min == distanceLeft) {
+            return new Point2D.Double(rectX, y);
+        }
+
         return new Point2D.Double(right, y); // else...
     }
-    
-    private boolean pointInsideEllipse(Point2D.Double ellipseCenter, double semiMajorLength, double semiMinorLength, Point2D.Double point)
-    {
+
+    private boolean pointInsideEllipse(Point2D.Double ellipseCenter, double semiMajorLength, double semiMinorLength, Point2D.Double point) {
         double xPart = Math.pow(point.x - ellipseCenter.x, 2) / Math.pow(semiMajorLength, 2);
         double yPart = Math.pow(point.y - ellipseCenter.y, 2) / Math.pow(semiMinorLength, 2);
-        
+
         return (xPart + yPart) <= 1;
     }
 }
