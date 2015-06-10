dec6fc18155577fa67e142b808906f747c1c0781
codeNinja77 <hightechninja7@gmail.com>
2015-06-09 18:54:09 -0700

    Start on GameEntity

---
+++ OmegaCentauri/src/MainPackage/GameEntity.java
@@ -0,0 +1,27 @@
+package MainPackage;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+/**
+ * @author Michael Kieburtz
+ */
+public abstract class GameEntity {
+    private Point2D.Double location;
+    private Point2D.Double faceAngle;
+    private ArrayList<BufferedImage> images;
+    private Explosion explosion;
+    
+    
+    public void update()
+    {
+        
+    }
+    
+    public void draw(Graphics2D g2d)
+    {
+        
+    }
+}
